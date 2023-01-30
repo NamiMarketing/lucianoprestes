ac6dab584f59679a91e61dd1d724b2a28c8136c0
FeruYasu <fernando_yasumoto@hotmail.com>
2023-01-29 21:35:46 -0300

    first version

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,10 @@
 const { DateTime } = require("luxon");
 
-module.exports = function (eleventyConfig) {
+module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/css");
   eleventyConfig.addPassthroughCopy("./src/img");
   eleventyConfig.addPassthroughCopy("./src/admin");
+  eleventyConfig.addPassthroughCopy("./src/fonts");
 
   eleventyConfig.addFilter("postDate", dateObj => {
     return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
