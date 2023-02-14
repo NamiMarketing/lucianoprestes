96867c372aedcb6f4c89f678a779c07beb9a6ad5
FeruYasu <fernando_yasumoto@hotmail.com>
2023-02-14 14:58:43 -0300

    fixed imags

--- webp.mjs
+++ webp.mjs
@@ -1,13 +1,10 @@
-import imagemin from 'imagemin';
-import imageminWebp from 'imagemin-webp';
-
+import imagemin from "imagemin";
+import imageminWebp from "imagemin-webp";
 
 (async () => {
-  const files = await imagemin(['src/img/*.{jpg,png}'], {
-    destination: 'src/img',
-    plugins: [
-      imageminWebp({ quality: 80 })
-    ]
+  const files = await imagemin(["src/img/*.{jpg,png}"], {
+    destination: "src/img",
+    plugins: [imageminWebp({ quality: 80 })]
   });
 
   console.log(files);
