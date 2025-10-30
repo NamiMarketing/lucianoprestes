a0567b468f29f16d8c89cf99e4b65db00aa2f054
marcelocho <marcelo.cho@gmail.com>
2025-10-30 13:11:28 -0300

    new lp

--- .eleventy.js
+++ .eleventy.js
@@ -1,49 +1,25 @@
-const { DateTime } = require("luxon");
-
-function shuffleArray(array) {
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
-  }
-
-  return array;
-}
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/css");
   eleventyConfig.addPassthroughCopy("./src/img");
-  eleventyConfig.addPassthroughCopy("./src/admin");
-  eleventyConfig.addPassthroughCopy("./src/fonts");
 
-  eleventyConfig.addFilter("postDate", (dateObj) => {
-    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
-  });
-
-  eleventyConfig.addNunjucksFilter("limit", (arr, limit) =>
-    arr?.slice(0, limit)
-  );
-
-  //O primeiro argumento são os proprios posts, não há necessidade de passar eles na função
-  eleventyConfig.addNunjucksFilter(
-    "getRandom",
-    (items, currentPage, quantity) => {
-      if (!items?.length || items?.length < 2) return;
-
-      let myItems = items?.filter((i) => {
-        return i.url !== currentPage.url;
-      });
-
-      const shuffledPosts = shuffleArray(myItems);
-
-      if (myItems.length === 0) return;
-      return shuffledPosts.slice(0, quantity);
-    }
-  );
+ eleventyConfig.setServerOptions({
+    middleware: [
+        function (req, res, next) {
+            if (req.statusCode === 404) {
+                res.writeHead(200, { "Content-Type": "text/html" });
+                res.end(require("fs").readFileSync("_site/404.html"));
+            } else {
+                next();
+            }
+        }
+    ]
+});
 
   return {
     dir: {
       input: "src",
-      output: "public",
-    },
+      output: "public"
+    }
   };
 };
